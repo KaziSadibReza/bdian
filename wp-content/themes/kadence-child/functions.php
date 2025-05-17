6f9396baf4f294c133da9a57e8ca5eaf9da83c25
Kazi Sadib Reza <kazisadibreza@gmail.com>
2025-05-17 21:37:39 +0600

    buy course button text chenge

--- wp-content/themes/kadence-child/functions.php
+++ wp-content/themes/kadence-child/functions.php
@@ -42,13 +42,13 @@ function woo_custom_change_cart_string($translated_text, $text, $domain) {
 // To change add to cart text on single product page
 add_filter( 'woocommerce_product_single_add_to_cart_text', 'woocommerce_custom_single_add_to_cart_text' );
 function woocommerce_custom_single_add_to_cart_text() {
-    return __( 'Buy Course', 'woocommerce' );
+    return __( 'Buy Now', 'woocommerce' );
 }
 
 // To change add to cart text on product archives(Collection) page
 add_filter( 'woocommerce_product_add_to_cart_text', 'woocommerce_custom_product_add_to_cart_text' );
 function woocommerce_custom_product_add_to_cart_text() {
-    return __( 'Buy Course', 'woocommerce' );
+    return __( 'Buy Now', 'woocommerce' );
 }
 
 
